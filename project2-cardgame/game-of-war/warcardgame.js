b4371ad0ac035d6aacaa87fefb8bcfd02baa5e85
George Chen <georgechen1338@gmail.com>
2022-03-03 09:40:42 -0500

    decided to go with a class for the game instead of a bunch of functions

--- project2-cardgame/game-of-war/warcardgame.js
+++ project2-cardgame/game-of-war/warcardgame.js
@@ -11,18 +11,9 @@ class Player {
   constructor(name, deck, lastPlayed) {
     this.name = name;
     this.deck = deck;
+    this.hand = [];
     this.lastPlayed = lastPlayed;
-  }
-
-  //takes top card of deck  
-  playCard() {
-    this.lastPlayed = this.deck[this.deck.length - 1]
-    return this.deck.shift();
-  }
-
-  //winner has both cards put back into the bottom of the deck
-  winCard(card) {
-    return this.deck.push(card);
+    this.playedCards = [];
   }
 }
 
@@ -37,9 +28,11 @@ class Deck {
     this.makeDeck();
   }
 
-  get fullDeck() {
-    return this.cards.length;
-  }
+  //was used to find the amount of cards in the array of cards as opposed to hard coding a length
+  //couldn't get it to work so I hard coded instead
+  // get fullDeck() {
+  //   return this.cards.length;
+  // }
 
   //make a deck consisting of 52 cards of 4 different suits, 13 different scores and ranks
   makeDeck() {
@@ -73,36 +66,81 @@ class Deck {
   }
 }
 
+//Cleaner to put the play game function into a class
+class Game {
+  constructor(round, pile, players, deck) {
+    this.round = 0;
+    this.pile = [];
+    this.players = [new Player("Fast Draw McGee"), new Player("Wild Wild West")];
+    this.deck = new Deck();
+    this.deck.shuffleDeck();
+  }
+
+  //deal cards to both players in sequence using shift to put the dealt card on the top of the pile each time
+  //a card is dealt
+  dealCards() {
+    for (let i = 0; i < this.deck.length; i++) {
+      if (i % 2 === 0) {
+        this.players[0].hand.unshift(this.deck.cards[i])
+      } else {
+        this.players[i].hand.unshift(this.cards.cards[i])
+      }
+    }
+    this.deck = [];
+  }
+
+  //each player plays a card from the top of their hand (an array) into a new array called played cards so they can
+  //be compared, the played card is then removed from the hands of both players and returned as last played
+  playCard() {
+    this.lastPlayed = this.deck[this.deck.length - 1]
+    this.players[0].playedCards.unshift(this.players[0].hand[0]);
+    this.players[1].playedCards.unshift(this.players[1].hand[0]]);
+    this.players[0].hand.shift();
+    this.players[1].hand.shift();
+  }
+
+  //starts game and adds 1 to the round counter
+  playGame() {
+    this.round += 1;
+    this.playCard();
+  }
+
+  //winner has both cards put back into the bottom of the deck
+  winCard(card) {
+    return this.deck.push(card);
+  }
+}
+
 //Starts game off by making a 52 card deck, shuffles it and then gives equal halves to the two players.
 //Runs the games by comparing the score value of the cards and also dealing with declaring war when two of
 //same cards are played
-
 playGame();
 
-function playGame() {
-  const deck = new Deck();
-  deck.shuffleDeck();
+// function playGame() {
+//   const deck = new Deck();
+//   deck.shuffleDeck();
 
-  //Split the randomly generated deck into 2 equal halves for the players
-  // const halfDeck = Math.ceil(deck.fullDeck / 2);
-  let p1Deck = new Player("PlayerOne", deck.cards.slice(0, 26));
-  let p2Deck = new Player("PlayerTwo", deck.cards.slice(26, 52));
-  p1Deck.playCard()
-  console.log(p2Deck.deck.length);
-  console.log(p1Deck.lastPlayed)
-  //console.log(p1Deck.name, p1Deck.deck);
-  //console.log(p2Deck.name, p2Deck.deck);
-}
+//   //Split the randomly generated deck into 2 equal halves for the players
+//   // const halfDeck = Math.ceil(deck.fullDeck / 2);
+//   let p1Deck = new Player("PlayerOne", deck.cards.slice(0, 26));
+//   let p2Deck = new Player("PlayerTwo", deck.cards.slice(26, 52));
+//   p1Deck.playCard()
+//   console.log(p2Deck.deck.length);
+//   console.log(p1Deck.lastPlayed)
+//   //console.log(p1Deck.name, p1Deck.deck);
+//   //console.log(p2Deck.name, p2Deck.deck);
+//   console.log(deck.cards);
+// }
 
 //each player plays 1 card
-function playingRound(playerOne, playerTwo) {
+//function playingRound(playerOne, playerTwo) {
+//
+//}
 
-}
-
-function whoWon() {
-  //pseudocode 
-  if p1Deck.lastplayed.score > p2deck.lastplayed.score;
-}
+//function whoWon() {
+//pseudocode
+//  if p1Deck.lastplayed.score > p2deck.lastplayed.score;
+//}
 //let deck = new Deck();
 //deck.makeDeck();
 //console.log(deck.cards);
